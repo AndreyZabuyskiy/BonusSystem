91ffe0da8a6014b3bc2f1a9c6850c8b804272f87
AndreyZabuyskiy <57301364+AndreyZabuyskiy@users.noreply.github.com>
2021-03-12 20:02:25 +0200

    Added CardNotFoundException

---
+++ BonusSystem/Models/Exceptions/CardNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonusSystem.Models.Exceptions
+{
+    public class CardNotFoundException : Exception { }
+}
